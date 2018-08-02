abec145fab9cb29eebad749330ab711aab69329f
Tony Wahula <mawahula4@gmail.com>
2018-08-02 16:23:25 -0600

    add card to array after being clicked

--- js/app.js
+++ js/app.js
@@ -1,38 +1,33 @@
 /*
  * Create a list that holds all of your cards
  */
-/* function shuffleClasses() {
-    let cardClasses = [];
-    var container = document.querySelector(".deck");
-    var matches = container.querySelectorAll("li.card > i");
-    matches.forEach(function(e) {
-        let classIndex = Math.floor(Math.random() * cardClasses.length);
-        e.classList.add(cardClasses[classIndex]);
-    });
-}
-shuffleClasses(); */
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+// select each card in the deck and add classes after being clicked
 let inputs = document.querySelectorAll('li');
+let cardArray = [];
 
 inputs.forEach(function(input) {
   input.addEventListener('click', function click() {
-    input.classList.add("match")
+    input.classList.add("open", "show")
+    //makeArray();
+    console.log(input.childNodes[1].classList.value.toString());
+    // trying to add the class name of i tags for each li
+    cardArray.push(input.childNodes[1].classList.value);
+    console.log(cardArray);
   });
 });
- //document.querySelector("li").addEventListener("click", flip);
 
- //var container = document.querySelector(".deck");
- //var matches = container.querySelectorAll("li.card > i");
+//function makeArray() {
+    
+//}
 
-
-
-
-
+// create an array of items with the class 'card', then run the shuffle function 
+// after the array is shuffled append the items back to the deck
 function init() {    
     const deck = document.querySelector('ul');
     let card = document.getElementsByClassName('card');
@@ -46,7 +41,7 @@ init();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
